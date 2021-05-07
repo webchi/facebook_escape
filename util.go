8ae43f93671a1b14bc7f7524b74698b953a6a458
Vasiliy Shakhunov <v.shakhunov@gmail.com>
2021-05-07 23:37:35 +0300

    Add .env

--- util.go
+++ util.go
@@ -17,13 +17,13 @@ type (
 func mustEnv() *Env {
 	appID := os.Getenv("FACEBOOK_APP_ID")
 	appSecret := os.Getenv("FACEBOOK_APP_SECRET")
-	baseURL := os.Getenv("SWITCHFEED_BASE_URL")
-	listenAddr := os.Getenv("SWITCHFEED_LISTEN_ADDR")	
+	baseURL := os.Getenv("BASE_URL")
+	listenAddr := os.Getenv("LISTEN_ADDR")	
 	if listenAddr == "" {
 		listenAddr = ":8080"
 	}
 	if appID == "" || appSecret == "" || baseURL == "" {
-		panic("Didn't defined 'FACEBOOK_APP_ID', 'FACEBOOK_APP_SECRET', 'SWITCHFEED_BASE_URL'")
+		panic("Didn't defined 'FACEBOOK_APP_ID', 'FACEBOOK_APP_SECRET', 'BASE_URL'")
 	}
 	return &Env{
 		AppID:      appID,
